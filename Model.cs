5612be9d3cd898fe43d71f5a06c84cbe838a791a
Jason De Lorme <jasondel@google.com>
2021-08-06 15:45:05 +0000

    Finished stable.

--- Model.cs
+++ Model.cs
@@ -5,7 +5,7 @@ namespace PricingApp
 {
     public class Model
     {
-        public record PerMinute(int Minute, double Vcpu, double GibMemory, double Price);
+        public record PerMinute(int Minute, double Vcpu, double GibMemory, int Requests, double Price);
         public record Price(double PerMonth, List<PerMinute> PerMinute);
         public Price Steady(double Vcpu, double GibMemory, double requestsPerSecond)
         {
@@ -18,11 +18,15 @@ namespace PricingApp
             perMinute.Add(new PerMinute(1, 
                 request.VcpuPerRequest, 
                 request.GiBMemoryPerRequest, 
+                (int)(requestsPerSecond * 60),
                 price));
             
             for (int i = 1; i < (Calculator.SecondsPerMonth/60); i++)
             {
-                perMinute.Add(perMinute[i-1] with { Minute = i });
+                perMinute.Add(perMinute[i-1] with { 
+                    Minute = i,
+                    Requests = (int)(requestsPerSecond * i)
+                });
             }
             
             var result = new Price(perMinute.Sum(p => p.Price), perMinute);
