b4b3a40e743dc2b1639826fd25dd12d92ad3d28f
Jason De Lorme <jasondel@google.com>
2021-08-06 18:28:02 +0000

    Work in progress.

--- Model.cs
+++ Model.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Collections.Generic;
 
@@ -31,6 +32,58 @@ namespace PricingApp
             
             var result = new Price(perMinute.Sum(p => p.Price), perMinute);
             return result;
+        }
+
+        public Price Bursty(double Vcpu, double GibMemory, double averageRequestsPerSecond)
+        {
+            // average
+            // peak
+            // trough
+            /*
+                var rpm = 3 * 60;
+                var k = .05;
+                const r = [...Array(1000).keys()];
+                r.forEach(i => data.addRow([i, (rpm * Math.sin(i*k))+rpm, 0]));
+            */
+
+            // Calculate per Minute.
+            const int MonthlyMinutes = (int)(730 * 60);
+            const double k = 0.05;
+            List<PerMinute> perMinute = new List<PerMinute>();
+            int LastRequests(int minute)
+            {
+                if (minute > 1)
+                    return perMinute[minute-2].Requests;
+                else
+                    return 0;
+            }
+
+            for (int minute = 1; minute < MonthlyMinutes; minute++)
+            {
+                double averageRequestsPerMinute = averageRequestsPerSecond * 60;
+                int requestsPerMinute = (int)((averageRequestsPerMinute * Math.Sin(minute * k)) + 
+                    averageRequestsPerMinute);
+                double requestsPerSecond = requestsPerMinute / 60;
+                
+                var request = new RequestProfile(Vcpu / requestsPerSecond, GibMemory / requestsPerSecond);
+                var calculator = new Calculator();
+                double price = calculator.PerSecond(request) * 60;
+
+                perMinute.Add(new PerMinute(minute, 
+                    request.VcpuPerRequest, 
+                    request.GiBMemoryPerRequest, 
+                    LastRequests(minute) + requestsPerMinute,
+                    price));
+            }
+            
+            var result = new Price(perMinute.Sum(p => p.Price), perMinute);
+            return result;
+        }
+
+        // Assumes exponential growth
+        public Price Growth(double Vcpu, double GibMemory, double requestsPerSecond)
+        {
+            return null;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 using PricingApp;
@@ -7,6 +8,8 @@ var app = builder.Build();
 
 var model = new Model();
 
-app.MapGet("/", () => model.Steady(1.0, 3.0, 1));
+app.MapGet("/", () => File.ReadAllText("index.html"));
+app.MapGet("/steady", () => model.Steady(1.0, 3.0, 1));
+app.MapGet("/bursty", () => model.Bursty(1.0, 3.0, 1));
 
 app.Run();
